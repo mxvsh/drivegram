d0731bf81df23155ce0f424cfa38ff5577101645
mxvsh <hi@monawwar.io>
2024-05-06 16:49:32 +0530

    fix: add missing page

---
+++ app/add-account/page.tsx
@@ -0,0 +1,11 @@
+import AddAccount from '#/lib/components/accounts/add';
+
+function Page() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-muted/50">
+      <AddAccount />
+    </div>
+  );
+}
+
+export default Page;
